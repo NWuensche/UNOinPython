933e27384f3a0140eb455c4e9f2ef67d9ecc75bd
Niklas Wuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-02-10 17:54:56 +0100

    add values for cards enum

--- Card.py
+++ Card.py
@@ -6,3 +6,20 @@ class Color(Enum):
     red = 1
     yellow = 2
     green = 3
+
+
+class Value(Enum):
+    zero = 0
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    draw2 = 11
+    skip = 12
+    revese = 13
