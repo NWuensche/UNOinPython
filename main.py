05cd76168f0edd16f69243ac35ec4d080c18c344
Niklas Wuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-02-16 19:05:14 +0100

    look if special card was thrown, do what you have to do

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ direction = 1
 lastCard = Card.drawCard(cardStack, thrownCards)
 thrownCards.append(lastCard)
 names = Card.getNames(numberOfPlayers)
+drawCards = 0
 while Card.someoneWon(handsOfCards) == False:
     CurrentScreen.showCurrentScreen(
         handsOfCards[whichPlayer], direction, lastCard, names[whichPlayer])
     # objekte mit 5 objteken machen
     lastCard = Card.playCard(handsOfCards[whichPlayer], lastCard, cardStack,
                             False, names[whichPlayer], direction, thrownCards)
+    if lastCard.getValue() == "draw2":
+        drawCards += 2
+    elif lastCard.getValue() == "reverse":
+        direction *= -1
+    elif lastCard.getValue() == "skip":
+        whichPlayer = (whichPlayer + 1) % numberOfPlayers
     thrownCards.append(lastCard)
     whichPlayer = (whichPlayer + 1) % numberOfPlayers
